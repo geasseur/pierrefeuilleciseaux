8b226008a34c8c04d30a7c04f87860cddda956a9
geasseur <baptiste.ventura@neuf.fr>
2017-07-10 11:47:52 +0200

    img resized, setTimeout for nouveauTour update, set timeout added for message, annonce css, dom modified, mainJoueur and mainOrdi change class to id

--- js/pierrefeuilleciseaux.js
+++ js/pierrefeuilleciseaux.js
@@ -50,12 +50,13 @@ function choixOrdi(){
 }
 
 //fonction de selection des cartes du joueur
-
+function test () {
+  alert("Nouveau Tour");
+}
 function choixCiseaux(){
     joueur = "ciseaux";
     document.getElementById("ciseauxPlateauJoueur").style.display = "inline-block";
     choixOrdi();
-    alert("ordi : " + ordi + " joueur : "+joueur);
     confrontation();
   }
 
@@ -63,7 +64,6 @@ function choixPierre(){
     joueur = "pierre";
     document.getElementById("pierrePlateauJoueur").style.display = "inline-block";
     choixOrdi();
-    alert("ordi : " + ordi + " joueur : "+joueur);
     confrontation();
   }
 
@@ -71,77 +71,80 @@ function choixFeuille(){
     joueur = "feuille";
     document.getElementById("feuillePlateauJoueur").style.display = "inline-block";
     choixOrdi();
-    alert("ordi : " + ordi + " joueur : "+joueur);
     confrontation();
 }
 
+
 function nouveauTour(){
+
   if(pointOrdi == 3 || pointJoueur == 3){
-    if(pointJoueur == 3){
-      alert("victoire du joueur ! ");
-      return "";
-    }
-    else if(pointOrdi == 3){
-      alert("victoire de l'ordinateur ! ");
-      return "";
-    }
+    document.getElementById("ordiGagne").style.display = "none";
+    document.getElementById("joueurGagne").style.display = "none";
+    document.getElementById("egalite").style.display = "none";
+    finJeu();
   }
   else {
     joueur ="";
     ordi = "";
     var listCarte = document.getElementsByClassName("cartePlateau");
+    //effacement des cartes du tableau
     for(i = 0; i < listCarte.length; i++){
       listCarte[i].style.display = "none"
     }
+    //effacement du panneaux central
+    document.getElementById("ordiGagne").style.display = "none";
+    document.getElementById("joueurGagne").style.display = "none";
+    document.getElementById("egalite").style.display = "none";
     pcf();
   }
 }
 
 function confrontation(){
   if(joueur=="ciseaux" && ordi=="pierre"){
-    alert("ordi gagne, la manche");
     pointOrdi = pointOrdi + 1;
-    document.getElementById("ordiGagne").style.display = "inline-block"
-    setTimeout(nouveauTour(), 3000);
+    document.getElementById("ordiGagne").style.display = "inline-block";
+    setTimeout(test, 1000);
+    setTimeout(nouveauTour, 1000);
   }
 
   else if(joueur=="pierre" && ordi=="feuille"){
-    alert("ordi gagne, la manche");
     pointOrdi = pointOrdi + 1;
-    document.getElementById("ordiGagne").style.display = "inline-block"
-    setTimeout(nouveauTour(), 3000);
+    document.getElementById("ordiGagne").style.display = "inline-block";
+    setTimeout(test, 1000);
+    setTimeout(nouveauTour, 1000);
   }
 
   else if(joueur=="feuille" && ordi=="ciseaux"){
-    alert("ordi gagne, la manche");
     pointOrdi = pointOrdi + 1;
-    document.getElementById("ordiGagne").style.display = "inline-block"
-    setTimeout(nouveauTour(), 3000);
+    document.getElementById("ordiGagne").style.display = "inline-block";
+    setTimeout(test, 1000);
+    setTimeout(nouveauTour, 1000);
   }
 
   else if(joueur=="ciseaux" && ordi=="feuille"){
-    alert("joueur gagne, la manche");
     pointJoueur = pointJoueur + 1;
-    document.getElementById("joueurGagne").style.display = "inline-block"
-    setTimeout(nouveauTour(), 3000);
+    document.getElementById("joueurGagne").style.display = "inline-block";
+    setTimeout(test, 1000);
+    setTimeout(nouveauTour, 1000);
   }
 
   else if(joueur=="feuille" && ordi=="pierre"){
-    alert("joueur gagne, la manche");
     pointJoueur = pointJoueur + 1;
-    document.getElementById("joueurGagne").style.display = "inline-block"
-    setTimeout(nouveauTour(), 3000);
+    document.getElementById("joueurGagne").style.display = "inline-block";
+    setTimeout(test, 1000);
+    setTimeout(nouveauTour, 1000);
   }
 
   else if(joueur=="pierre" && ordi =="ciseaux"){
-    alert("joueur gagne, la manche");
     pointJoueur = pointJoueur + 1;
-    document.getElementById("joueurGagne").style.display = "inline-block"
-    setTimeout(nouveauTour(), 3000);
+    document.getElementById("joueurGagne").style.display = "inline-block";
+    setTimeout(test, 1000);
+    setTimeout(nouveauTour, 1000);
   }
   else{
-    alert("egalite");
-    setTimeout(nouveauTour(), 3000);
+    document.getElementById("egalite").style.display = "inline-block";
+    setTimeout(test, 1000);
+    setTimeout(nouveauTour, 1000);
   }
 }
 
@@ -158,71 +161,15 @@ function pcf(){
     }
   }
 
-
-
-
-/*function pcf(){
-
-
-  while(pointJoueur<4 && pointOrdi<4){
-
-    ordi=Math.trunc(Math.random()*100);
-    joueur=prompt("Quel signe?pierre, ciseaux ou feuille");
-    if(ordi <= 33){
-      ordi="ciseaux";
-    }
-
-    else if(ordi>33 && ordi<=66){
-      ordi = "pierre";
-    }
-
-    else{
-      ordi = "feuille";
-    }
-
-    if(joueur===ordi){
-      console.log("égalité");
-    }
-
-    else if(joueur=="ciseaux" && ordi=="pierre"){
-      console.log("ordi gagne, la manche");
-      pointOrdi = pointOrdi + 1;
-    }
-
-    else if(joueur=="pierre" && ordi=="feuille"){
-      console.log("ordi gagne, la manche");
-      pointOrdi = pointOrdi + 1;
-    }
-
-    else if(joueur=="feuille" && ordi=="ciseaux"){
-      console.log("ordi gagne, la manche");
-      pointOrdi = pointOrdi + 1;
-    }
-
-    else if(joueur=="ciseaux" && ordi=="feuille"){
-      console.log("joueur gagne, la manche");
-      pointJoueur = pointJoueur + 1;
-    }
-
-    else if(joueur=="feuille" && ordi=="pierre"){
-      console.log("joueur gagne, la manche");
-      pointJoueur = pointJoueur + 1;
-    }
-
-    else if(joueur=="pierre" && ordi =="ciseaux"){
-      console.log("joueur gagne, la manche");
-      pointJoueur = pointJoueur + 1;
-    }
-
-    else{
-      console.log("vous avez mal rentrez un signe");
-    }
-    if(pointJoueur==4){
-      console.log("victoire du joueur ! ");
-    }
-    else if(pointOrdi==4){
-      console.log("victoire de l'ordinateur ! ");
-    }
+function finJeu(){
+  if(pointJoueur == 3){
+    document.getElementById("victoire").style.display = "inline-block";
+    document.getElementById("mainOrdi").style.display = "none";
+    document.getElementById("mainJoueur").style.display = "none";
+  }
+  else if(pointOrdi == 3){
+    document.getElementById("defaite").style.display = "inline-block";
+    document.getElementById("mainOrdi").style.display = "none";
+    document.getElementById("mainJoueur").style.display = "none";
   }
 }
-*/
